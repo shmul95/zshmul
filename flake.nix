a5140ec5e0034434a5d17418fefe3258a8dcf28b
shmul95 <samuel.gross@epitech.eu>
2026-02-28 16:06:05 +0100

    feat(flake): creating a homeManager module instead

--- flake.nix
+++ flake.nix
@@ -4,82 +4,52 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     typewritten-theme = { url = "github:reobin/typewritten"; flake = false; };
+    tshmux.url = "github:shmul95/tshmux";
   };
 
-  outputs = { self, nixpkgs, typewritten-theme }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+  outputs = { self, nixpkgs, typewritten-theme, tshmux }: {
+    homeManagerModules.default = { config, pkgs, ... }: {
+      home.packages = with pkgs; [
+        tshmux.packages.${pkgs.system}.default
+        lazygit bat
+      ];
 
-      # This creates the data directory
-      zshHome = pkgs.runCommand "zshmul-custom" {} ''
-        # Create a "custom" style directory
-        mkdir -p $out/plugins/zsh-syntax-highlighting
-        mkdir -p $out/plugins/zsh-autosuggestions
-        mkdir -p $out/plugins/z
-        mkdir -p $out/plugins/git
-        mkdir -p $out/themes
+      programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        autosuggestions.enable = true;
+        syntaxHighlighting.enable = true;
 
-        # 1. Link the external "custom" plugins
-        # Note: We link the .zsh file to .plugin.zsh so OMZ's loader finds it
-        ln -s ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh \
-              $out/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+        sessionVariables = {
+          EDITOR = "nvim";
+          VIRTUAL_ENV_DISABLE_PROMPT = 1;
+          HYPHEN_INSENSITIVE = "true";
 
-        ln -s ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh \
-              $out/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+          TYPEWRITTEN_PROMPT_LAYOUT = "singleline";
+          TYPEWRITTEN_SYMBOL = "$";
+          TYPEWRITTEN_ARROW_SYMBOL = "->";
+          TYPEWRITTEN_RELATIVE_PATH = "adaptive";
+          TYPEWRITTEN_CURSOR = "terminal";
+        };
 
-        # 2. Grab 'z' and 'git' from the main OMZ package
-        cp -r ${pkgs.oh-my-zsh}/share/oh-my-zsh/plugins/z/* $out/plugins/z/
-        cp -r ${pkgs.oh-my-zsh}/share/oh-my-zsh/plugins/git/* $out/plugins/git/
+        oh-my-zsh = {
+          enable = true;
+          plugins = [ "git" "z" ];
+          theme = "typewritten";
+        };
 
-        # 3. Link the theme
-        ln -s ${typewritten-theme} $out/themes/typewritten
-        ln -s ${typewritten-theme}/typewritten.zsh-theme $out/themes/typewritten.zsh-theme
-      '';
+        shellAliases = {
+          l = "ls -la";
+          lg = "lazygit";
+          nd = "nix develop";
+        };
 
-      # Update zshrc variables
-      zshrc = pkgs.replaceVars ./zshrc {
-        zshhome = "${pkgs.oh-my-zsh}/share/oh-my-zsh"; # The core logic stays in the Nix Store
-        zshcustom = "${zshHome}";                      # Your custom folder is your result
-      };
-
-      # Enhance the existing zshrc with PATH for our tools
-      enhancedZshrc = pkgs.writeText "enhanced-zshrc" ''
-        # Add nix-provided tools to PATH
-        export PATH="${pkgs.lib.makeBinPath (with pkgs; [ git lazygit neovim ])}:$PATH"
-        
-        # Source the main configuration
-        source ${zshrc}
-      '';
-
-      # The actual script - use proper temp directories with better cleanup
-      zshmul = pkgs.writeShellScriptBin "zshmul" ''
-        # Use proper temporary directory (will be cleaned up by system)
-        ZSHMUL_DIR=$(mktemp -d -t zshmul.XXXXXX)
-        
-        # Copy our zshrc to the temp directory
-        cp ${enhancedZshrc} "$ZSHMUL_DIR/.zshrc"
-        
-        # Set ZDOTDIR and launch zsh
-        export ZDOTDIR="$ZSHMUL_DIR"
-        
-        # Multiple cleanup strategies
-        cleanup() {
-          rm -rf "$ZSHMUL_DIR" 2>/dev/null || true
-        }
-        
-        # Cleanup on various signals and exit
-        trap cleanup EXIT TERM INT QUIT
-        
-        # Launch zsh 
-        exec ${pkgs.zsh}/bin/zsh "$@"
-      '';
-
-    in {
-      # This combines the bin and the data so they BOTH appear in 'result'
-      packages.${system}.default = pkgs.symlinkJoin {
-        name = "zshmul";
-        paths = [ zshmul zshHome ];
+        plugins = [{
+            name = "typewritten";
+            file = "typewritten.zsh-theme";
+            src = typewritten-theme;
+        }];
       };
     };
+  };
 }
