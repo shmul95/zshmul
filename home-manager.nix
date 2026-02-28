6ac24e9abf9ca75d744da7ff9ac3e36a72a6deca
shmul95 <samuel.gross@epitech.eu>
2026-02-28 18:17:18 +0100

    feat(modularize): modularize the different output into multiple files

---
+++ home-manager.nix
@@ -0,0 +1,52 @@
+# home-manager.nix - Home Manager module configuration
+{ typewritten-theme, tshmux }:
+
+{ config, pkgs, ... }: {
+  home.packages = with pkgs; [
+    tshmux.packages.${pkgs.system}.default
+    lazygit bat # add more pkgs here
+  ];
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    sessionVariables = {
+      EDITOR = "nvim";
+      VIRTUAL_ENV_DISABLE_PROMPT = 1;
+      HYPHEN_INSENSITIVE = "true";
+
+      TYPEWRITTEN_PROMPT_LAYOUT = "singleline";
+      TYPEWRITTEN_SYMBOL = "$";
+      TYPEWRITTEN_ARROW_SYMBOL = "->";
+      TYPEWRITTEN_RELATIVE_PATH = "adaptive";
+      TYPEWRITTEN_CURSOR = "terminal";
+    };
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "z" ];
+    };
+
+    shellAliases = {
+      l = "ls -la";
+      lg = "lazygit";
+      nd = "nix develop";
+    };
+
+    plugins = [{
+        name = "typewritten";
+        file = "typewritten.zsh-theme";
+        src = typewritten-theme;
+    }];
+
+    initContent = ''
+      # Automatically launch tshmux if we are in an interactive session and not already in a mux session
+      if command -v tshmux >/dev/null 2>&1 && [[ -z "$TMUX" && $- == *i* && -t 1 ]]; then
+        tshmux
+      fi
+    '';
+  };
+}
